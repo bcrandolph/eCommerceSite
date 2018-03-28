814ac6c1f755add69bf4a7cf684e8b6162282fd3
bcrandolph <bcrandolph@crimson.ua.edu>
2018-03-28 18:07:18 -0500

    Add project files.

---
+++ eCommerceSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eCommerceSite.Startup))]
+namespace eCommerceSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
